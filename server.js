4b6b32a8c8397b0fd0ff7f071a2f5e3c8438567e
dasfef <mealhouse3377@gmail.com>
2023-06-24 18:05:43 +0900

    idk

--- server.js
+++ server.js
@@ -23,6 +23,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// 서버에 데이터를 보낼 때 
+app.post('/', function(req, res) {
+  var data = '';
+  req.setEncoding('binary');
+
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+
+  req.on('end', function() {
+    io.emit('binaryData', data);
+    res.end();
+  });
+});
+
 server.listen(8989, () => {
   console.log('listening on *:8989');
 });
