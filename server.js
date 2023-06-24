c54b87098c93e7509d9ec5b000aa45e5b3fdf1cd
dasfef <mealhouse3377@gmail.com>
2023-06-24 19:36:08 +0900

    Complete the yolo in tensorflow.js

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 const WebSocket = require('ws');
 const http = require('http');
 const express = require('express');
-const app = express();
+const { connected } = require('process');
 
+const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const maxClients = 5;                                                 // 동시 연결 수 제한
-let connectedClients = [];
+const maxClients = 10;                                                  // 동시 연결 수 제한 : 10
+let connectedClients = [];                                              // 클라이언트 리스트
+var orders = [];
 
+// 클라이언트 ID 파악 함수
+function generateUniqueID() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+// 웹소켓 접속시 index.html 파일 전송
+app.use("/", function(req, res){
+  res.sendFile(__dirname + '/public/index.html');                       
+});
+
+// 웹소켓 접속시
 wss.on('connection', (ws) => {
   if (connectedClients.length < maxClients){
-    connectedClients.push(ws);                                    // 새로 연결된 클라이언트를 배열에 추가
-    console.log('Client connected');
+    connectedClients.push(ws);                                          // 새로 연결된 클라이언트를 배열에 추가
+    ws.id = generateUniqueID();
+    orders.push(ws.id);
+    console.log('==== Client Connected ====');                                    // 클라이언트 ID 파악 후 로그 출력
+    console.log('Order : ', orders.indexOf(ws.id)+1);
+    console.log('ID : ', ws.id);
   } else {
-    ws.send('Server is at capacity, please try again later');
+    ws.send('Server is at capacity, please try again later');           // 클라이언트 접속 개수 초과시
     ws.close();
     return;
   }
 
+// 클라이언트로부터 받은 메세지를 다른 모든 클라이언트에게 전송
   ws.on('message', (message) => {
-    // 클라이언트로부터 받은 메세지를 다른 모든 클라이언트에게 전송
     connectedClients.forEach(client => {
       if(client !== ws && client.readyState === WebSocket.OPEN){
         client.send(message);
@@ -29,13 +46,16 @@ wss.on('connection', (ws) => {
   });
 
   ws.on('close', () => {
-    console.log('Client disconnected');
+    console.log('==== Client disconnected ====');
+    console.log('Order : ', orders.indexOf(ws.id)+1);
+    console.log('ID : ', ws.id);
+    orders = orders.filter(client => client !== ws.id);
     connectedClients = connectedClients.filter(client => client !== ws);
   });
 });
 
-server.listen(8999, () => {
-  console.log('Server started on Port 8999');
+server.listen(8989, () => {
+  console.log('Server started on Port 8989');
 });
 
 
