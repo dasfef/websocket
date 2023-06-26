e49fda7a41f789d6ddc76c606a7ab84e87d0ffdb
dasfef <mealhouse3377@gmail.com>
2023-06-26 20:43:51 +0900

    Image capture succeed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const WebSocket = require('ws');
 const express = require('express');
 const http = require('http');
-const express = require('express');
 const { connected } = require('process');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +11,7 @@ const wss = new WebSocket.Server({ server });
 const maxClients = 10;                                                  // 동시 연결 수 제한 : 10
 let connectedClients = [];                                              // 클라이언트 리스트
 var orders = [];
+let imgNum = 0;                                                         // 이미지 캡쳐 변수
 
 // 클라이언트 ID 파악 함수
 function generateUniqueID() {
@@ -38,6 +40,20 @@ wss.on('connection', (ws) => {
 
 // 클라이언트로부터 받은 메세지를 다른 모든 클라이언트에게 전송
   ws.on('message', (message) => {
+    // 이미지 캡쳐 받는 부분
+    // if(message instanceof Buffer){
+    //   setTimeout(function() {
+    //     fs.writeFile(`/Users/dasfef/Desktop/WORK/websocket/capture/image_${imgNum}.jpg`, message, err => {
+    //       if(err) {
+    //         console.log('Error: ', err);
+    //         return;
+    //       }
+    //       imgNum++;
+    //     });
+    //   }, 5000);
+    // }
+
+    // 이미 다른 클라이언트로 전송하는 부분
     connectedClients.forEach(client => {
       if(client !== ws && client.readyState === WebSocket.OPEN){
         client.send(message);
@@ -47,34 +63,16 @@ wss.on('connection', (ws) => {
 
   ws.on('close', () => {
     console.log('==== Client disconnected ====');
+    delete orders.indexOf(ws.id);
     console.log('Order : ', orders.indexOf(ws.id)+1);
     console.log('ID : ', ws.id);
-    orders = orders.filter(client => client !== ws.id);
+    // orders = orders.filter(client => client !== ws.id);
     connectedClients = connectedClients.filter(client => client !== ws);
   });
 });
 
-server.listen(8989, () => {
-  console.log('Server started on Port 8989');
-});
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
-
-// 서버에 데이터를 보낼 때 
-app.post('/', function(req, res) {
-  var data = '';
-  req.setEncoding('binary');
-
-  req.on('data', function(chunk) {
-    data += chunk;
-  });
-
-  req.on('end', function() {
-    io.emit('binaryData', data);
-    res.end();
-  });
 });
 
 server.listen(8989, () => {
